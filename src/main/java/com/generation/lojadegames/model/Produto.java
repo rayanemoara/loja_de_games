f34c6b3073ee00bc59bfc039ddf90c19697a2c1b
rayane moara <rayane_moara@hotmail.com>
2024-12-05 15:41:58 -0300

    Backend Loja de Games

---
+++ src/main/java/com/generation/lojadegames/model/Produto.java
@@ -0,0 +1,98 @@
+package com.generation.lojadegames.model;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é obrigatório!")
+	private String nome;
+	
+	@NotBlank(message = "O atributo descrição é obrigatório!")
+	private String descricao;
+	
+	private LocalDate dataLancamento;
+	
+	private Double preco;
+	
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+	public LocalDate getDataLancamento() {
+		return dataLancamento;
+	}
+
+
+	public void setDataLancamento(LocalDate dataLancamento) {
+		this.dataLancamento = dataLancamento;
+	}
+
+
+	public Double getPreco() {
+		return preco;
+	}
+
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+}
